5ddc48ac6648d1cd989f15dce155d04ad69a074f
cerny <cerny@technikum-wien.at>
2022-04-19 09:27:43 +0200

    add Rectangle demo code

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,68 @@
+package shapes;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height){
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public Rectangle(double length) {
+        this(length, length);
+    }
+
+    public Rectangle(){
+        this(1, 1);
+    }
+
+    public Rectangle(Rectangle rect){
+        this.width = rect.width;
+        this.height = rect.height;
+    }
+
+    public void scale(double scaleW, double scaleH){
+        setWidth(width * scaleW);
+        setHeight(height * scaleH);
+    }
+
+    public void scale(double scale){
+        setWidth(width * scale);
+        setHeight(height * scale);
+    }
+
+    public void setWidth(double width){
+        if(width <= 0)
+            return;
+
+        this.width = width;
+    }
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        if(width < 0){
+            this.height = 0;
+            return;
+        }
+        if(width > 100){
+            this.height = 100;
+            return;
+        }
+
+        this.height = height;
+    }
+
+    public double getArea(){
+        return width * height;
+    }
+
+    public void print(){
+        System.out.printf("Rectangle (Width: %5.2f; Height: %5.2f; Area: %8.2f)\n", width, height, getArea());
+    }
+}
---
+++ src/shapes2/RectangleDemo.java
@@ -0,0 +1,22 @@
+package shapes2;
+
+import shapes.Rectangle;
+
+public class RectangleDemo {
+
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(10, 5);
+        Rectangle r2 = new Rectangle(14, 33);
+        Rectangle r3 = new Rectangle(6);
+        Rectangle r4 = new Rectangle(r2);
+        r2.scale(1.2, 0.5);
+        r4.scale(5);
+
+        //System.out.println(r2.getWidth() + " * " + r2.getHeight() + " = " + r2.getArea());
+        r1.print();
+        r2.print();
+        r3.print();
+        r4.print();
+    }
+}
